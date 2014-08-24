048c6e6169cc72e6ad73fd55fb7e978315c04c01
Anthony Laibe <anthony@laibe.cc>
2014-08-24 18:48:58 +0100

    Add response spec

---
+++ spec/signable/query/response_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe Signable::Query::Response do
+
+  describe "#ok?" do
+    let(:response) { Signable::Query::Response.new http_response }
+    subject { response.ok? }
+
+    context "when code is 200" do
+      let(:http_response) { double 'http_response', code: 200 }
+      it { should be true }
+    end
+
+    context "when code is !200" do
+      let(:http_response) { double 'http_response', code: 205 }
+      it { should be false }
+    end
+  end
+
+  describe "#object" do
+    let(:http_response) { double 'http_response', body: { bar: 'foo' }.to_json }
+
+    it "parse body response" do
+      response = Signable::Query::Response.new http_response
+      expect(response.object).to be_eql({ 'bar' => 'foo' })
+    end
+  end
+
+end
