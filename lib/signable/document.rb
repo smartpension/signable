fbce6c7fef7f29f2b09f28e116e764c851179f7e
Anthony Laibe <anthony@laibe.cc>
2014-10-20 15:45:30 +0100

    Allow document to be fetch

--- lib/signable/document.rb
+++ lib/signable/document.rb
@@ -1,6 +1,7 @@
 module Signable
   class Document
     include Signable::Concerns::Model
+    include Signable::Concerns::Query
 
     column :template_fingerprint, presence: true
     column :title, presence: true
