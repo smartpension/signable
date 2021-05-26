da19f3667d55d8c8fdd394340ffd96e283a83eaa
kaizencodes <daniel.kaizencodes@gmail.com>
2021-05-26 10:29:22 +0200

    [PINK-214] Add specs for remind endpoint

--- spec/signable/query/client_spec.rb
+++ spec/signable/query/client_spec.rb
@@ -1,32 +1,33 @@
 require "spec_helper"
 
 describe Signable::Query::Client do
-  subject(:client) do
-    dc = described_class.new
-      # described_class.define(:cattr_accessor, :foo)
-    dc.class.class_variable_set(:@@base_uri, 'api.signable.co.uk')
+  include_context 'when signable is configured'
 
-      # described_class.base_uri = 'api.signable.co.uk'
-    dc
-  end
+  subject(:client) { described_class.new }
 
-  # before do
-  #   # allow(described_class).to receive(:base_uri).and_return('api.signable.co.uk')
-  #   allow(Signable::Configuration).to receive(:base_url).and_return('api.signable.co.uk')
-  # end
+  describe '#remind' do
+    context 'when envelope exists' do
+      subject { client.remind('envelopes', '2a81e63c5b497761c60121f279c904a0') }
+      
+      it 'sends reminder', vcr: 'client/remind/success' do
+        response = subject
+        
+        expect(response).to be_instance_of(Signable::Query::Response)
+        expect(response.ok?).to eq true
+        expect(response.object["message"]).to eq 'The next signing party for this envelope has been reminded.'
+      end
+    end
 
-  # 'api.signable.co.uk'
+    context 'when envelope does not exist' do
+      subject { client.remind('envelopes', 'FINGERPRINT123456789') }
 
-  describe '#remind', vcr: {record: :once} do
-    # described_class.base_uri =
-
-    subject { client.remind('envelopes', 'FINGERPRINT123456789') }
-
-    it 'calls /remind Signable endpoint' do
-
-      # binding.pry
-      expect(subject).to be_instance_of(Signable::Query::Response)
+      it 'returns message saying the envelope does not exist.', vcr: 'client/remind/not_found' do
+        response = subject
+        
+        expect(response).to be_instance_of(Signable::Query::Response)
+        expect(response.ok?).to eq false
+        expect(response.object["message"]).to eq 'The envelope does not exist. Have you used the correct envelope fingerprint?'
+      end
     end
   end
-
 end
