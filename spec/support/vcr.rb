755237acef96a8e537eced7656c2058feb6dd905
Christian Ball <christianball@hotmail.co.uk>
2021-05-27 14:57:30 +0100

    Add default_cassette_options to VCR config.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,6 +6,10 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.default_cassette_options = {
+    decode_compressed_response: true,
+    allow_unused_http_interactions: false
+  }
 
   c.filter_sensitive_data('Bearer <OAUTH_TOKEN>') do |interaction|
     interaction.request.headers['Authorization']&.first
